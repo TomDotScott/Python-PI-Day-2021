58686af38258d1237eeda61046bf5a62bbe1fa25
TomDotScott <tom.scott45456@gmail.com>
2021-03-14 16:43:19 +0000

    Finding the gcd of numbers from the list

--- main.py
+++ main.py
@@ -16,11 +16,32 @@ def construct_array(fileName):
 def greatest_common_denominator(a, b):
     if b > a:
         b, a = a, b
+
+    if b == 0:
+        b = 1
+
     remainder = a % b
+
     if remainder == 0:
         return b
     else:
         return greatest_common_denominator(b, remainder)
 
 
-print(greatest_common_denominator(252, 105))
+numbersArray = construct_array('randomDigits.txt')
+# Count through the array in pairs
+totalNumbers = 0
+coPrimeCount = 0
+for i in range(0, len(numbersArray), 2):
+    a = numbersArray[i]
+    b = numbersArray[i + 1]
+    gcd = greatest_common_denominator(a, b)
+    print(str(a) + ", " + str(b) + " = " + str(gcd))
+    # If the gcd is 1, they are 'Co-Prime'
+    if gcd == 1:
+        coPrimeCount += 1
+
+    totalNumbers += 1
+
+print(totalNumbers)
+print(coPrimeCount)
