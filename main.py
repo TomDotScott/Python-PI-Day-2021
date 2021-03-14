f77ab3725f1516078ff4a3c7ad2411b59b48c047
TomDotScott <tom.scott45456@gmail.com>
2021-03-14 16:25:17 +0000

    Function definition

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-# Read the file
-file = open("randomDigits.txt", 'r')
-millionDigits = file.readline()
-# split into array of 5 digit numbers
-numbers = []
-for i in range(0, len(millionDigits), 5):
-    num = ""
-    for j in range(5):
-        num += millionDigits[i + j]
-    numbers.append(int(num))
+def constructArray(fileName):
+    # Read the file
+    file = open(fileName, 'r')
+    millionDigits = file.readline()
+    # split into array of 5 digit numbers
+    numbers = []
+    for i in range(0, len(millionDigits), 5):
+        num = ""
+        for j in range(5):
+            num += millionDigits[i + j]
+        numbers.append(int(num))
+    file.close()
+    return numbers
 
-print(numbers)
+
+print(constructArray('randomDigits.txt'))
